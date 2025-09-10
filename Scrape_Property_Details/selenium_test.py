2285a85bcd63aa0858a2d16c5f339b605739fd2f
huzxaifa <i210527@nu.edu.pk>
2025-09-11 00:15:22 +0500

    Remove unwanted files from repo, keep locally

--- Scrape_Property_Details/selenium_test.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-
-driver = webdriver.Chrome()  # or specify path: Service("path/to/chromedriver")
-driver.get("https://www.google.com")
-print(driver.title)
-driver.quit()
